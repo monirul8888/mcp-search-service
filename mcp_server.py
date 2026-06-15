aaec869eabb671ad70fa1a84c7dd4fd2f17cc73c
Monirul <monirul429588@gmail.com>
2026-06-15 10:02:28 +0600

    Added Async with Serper

--- mcp_server.py
+++ mcp_server.py
@@ -5,27 +5,46 @@
 import http.client
 import json
 import os
+import httpx
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+SERPER_URL = "https://google.serper.dev/search"
 
+async def search_web(query: str)-> dict | None:
+    
+    payload = json.dumps({
+    "q": query,
+    "num":2
+    })
+    
+    headers = {
+    'X-API-KEY': SERPER_API_KEY,
+    'Content-Type': 'application/json'
+    }
+    #conn = http.client.HTTPSConnection("google.serper.dev")
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            SERPER_URL,
+            headers=headers,
+            data = payload,
+            timeout=30.0
+        )
+        
+        response.raise_for_status()
+        return response.json()
+    
+        #conn.request("POST", "/search", payload, headers)
+        # res = conn.getresponse()
+        # data = res.read()
+        # return(data.decode("utf-8"))
 
-query = "ChromaDB official documentation"
+res = asyncio.run(search_web(query = "ChromaDB official documentation"))
 
-conn = http.client.HTTPSConnection("google.serper.dev")
-payload = json.dumps({
-  "q": query, "num":2
-})
-headers = {
-  'X-API-KEY': SERPER_API_KEY,
-  'Content-Type': 'application/json'
-}
-conn.request("POST", "/search", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+print(res)
 
 
 
